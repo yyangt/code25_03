e56cd31fd0610e4a7d462b1abb99e3a1214a6201
yyangt <lizhideduiyou@gmail.com>
2025-03-01 17:41:00 +0800

    Auto backup

--- 自动化脚本.py
+++ 自动化脚本.py
@@ -6,7 +6,7 @@ import time
 REPO_PATH = r"E:\Git\github"  # 替换为你的代码仓库路径
 GITHUB_REPO = "g1"        # 远程仓库名称
 COMMIT_MESSAGE = "Auto backup"  # 提交信息
-TIME_INTERVAL = "17:24"       # 定时时间（每天的 22:00）
+TIME_INTERVAL = "17:41"       # 定时时间（每天的 22:00）
 
 def git_push():
     """提交并推送代码到 GitHub"""
